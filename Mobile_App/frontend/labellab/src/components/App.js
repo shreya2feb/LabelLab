360a65718b6abdceea1ea82326200d3fb83fa93e
Deepak <shreya2feb@gmail.com>
2019-02-26 20:14:24 +0530

    ios .ipa tested

--- Mobile_App/frontend/labellab/src/components/App.js
+++ Mobile_App/frontend/labellab/src/components/App.js
@@ -4,7 +4,6 @@ import {
   Image,
 } from 'react-native';
 import { View,Container,Content, Button,Icon, Text,Header,Left,Body,Right,Title,Footer, FooterTab, Badge } from 'native-base';
-import MyHeader from './Header';
 
 
 class App extends Component{
@@ -85,28 +84,6 @@ class App extends Component{
         </Button>
           
         </Content>
-        <Footer>
-          <FooterTab>
-            <Button badge vertical>
-              <Badge><Text>2</Text></Badge>
-              <Icon name="apps" />
-              <Text>Apps</Text>
-            </Button>
-            <Button vertical>
-              <Icon name="camera" />
-              <Text>Camera</Text>
-            </Button>
-            <Button active badge vertical>
-              <Badge ><Text>51</Text></Badge>
-              <Icon active name="navigate" />
-              <Text>Navigate</Text>
-            </Button>
-            <Button vertical>
-              <Icon name="person" />
-              <Text>Contact</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
         </Container>
     );
   }
